ac0d185fbf6ec967a0c48fe91d9f824277b586eb
ismailfurkangonul <ismailfurkangonul2@gmail.com>
2022-08-07 19:33:02 +0300

    guncelleme yapıldııı

---
+++ 0.4.PredicateDelege/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0._4.PredicateDelege
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Customer> musteriler = new() {
+             new Customer{Id=0,Name="Furkan",Surname="Gonul"},
+             new Customer{Id=1,Name="Dries",Surname="Mertens"}
+             };
+            musteriler.Remove(musteriler.Find(x => x.Id == 0)); //predicate example
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+    internal class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+
+
+    }
+}
